20940df49038d2b2c5699f49294ffff41e4df3d6
0xrfsd <64338100+0xrfsd@users.noreply.github.com>
2021-02-03 01:28:32 -0300

    V0

---
+++ index.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+import { Press } from './styles'
+
+class Counter extends Component {
+    state = { count: 0 };
+
+    setCount = () => this.setState(
+      prevState => ({ ...prevState, count: this.state.count + 1 })
+    )
+
+    render() {
+        const { count } = this.state
+        return (
+            <View>
+                <Press onPress={this.setCount} />
+                <Text>{count}</Text>
+            </View>
+        );
+    }
+}
+
+export default class Guests extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Counter />
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#ecf0f1',
+    },
+});
---
+++ styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components/native'
+
+export const Wrapper = styled.SafeAreaView`
+  background: #fff;
+  flex: 1;
+`;
+
+export const Press = styled.TouchableOpacity`
+  height: 50px;
+  width: 50px;
+  background: #000;
+`;
